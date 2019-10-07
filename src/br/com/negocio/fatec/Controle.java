1af4a63df48700d17c412553081b5e35eddc511e
mflJefferson <mflJefferson@gmail.com>
2019-10-07 13:28:20 -0300

    Feat: Serialização

--- src/br/com/negocio/fatec/Controle.java
+++ src/br/com/negocio/fatec/Controle.java
@@ -43,38 +43,6 @@ public class Controle {
 		escritor.close();
 		
 	}
-//	public void carregar(Agenda ag) throws Exception {
-//		File arq = new File("agenda.txt");
-//		FileReader rd = new FileReader(arq);
-//		BufferedReader brd = new BufferedReader(rd);
-//		String texto = brd.readLine();
-//		while(texto != null) {
-//			Cliente c  = new Cliente(texto);
-//			ag.clientes.add(c);
-//			texto = brd.readLine();
-//		}
-//		brd.close();
-//	}
-//	
-//	public void salvar(Agenda ag) throws IOException {
-//		File arq = new File("agenda.txt");
-//		FileWriter wr = new FileWriter(arq);
-//		BufferedWriter bwr = new BufferedWriter(wr);
-//		for(Cliente c : ag.clientes) {
-//			bwr.write(c.getNome());
-//			bwr.newLine();
-//			bwr.write(c.getTelefone());
-//			bwr.newLine();
-//			bwr.write(c.getDtNascimento());
-//			bwr.newLine();
-//			bwr.write(c.getIdade());
-//			bwr.newLine();
-//			bwr.write(c.getGenero());
-//			bwr.newLine();
-//		}
-//		bwr.close();
-//	}
-	
 	
 	@Override
 	protected void finalize() throws Throwable {
